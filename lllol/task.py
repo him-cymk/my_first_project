33004eb8e4b034af9c6061288a6e2ecb5a5bf8f2
him-cymk <lolx404909@gmail.com>
2025-03-24 13:26:54 +0000

    Add files via upload

---
+++ lllol/task.py
@@ -0,0 +1,41 @@
+# """
+# age = int(input(" what is your age; "))
+# print("hello",age )
+
+
+# age = input("enter your name")
+# print("hello",age)
+
+
+
+
+
+# age = input("what is your name")
+# print("hello",age)
+# 
+"""
+name = input("my name is:")
+print("welcom", name )
+age =input("what is your age?")
+print("my age is:", age)
+"""
+no1 = int(input("what is your first number;" ))
+no2 = int(input("what is your second number;" ))
+
+r1 = no1 - no2
+r2 = no1 + no2
+r3 = no1 // no2
+r4 = no1 % no2
+r5 = no1 ** no2
+r5 = no1 * no2
+r6 = no1 / no2
+
+print("no1 - no2 = ", r1)
+print("no1 + no2 = ", r2)
+print("no1 // no2 = ", r3)
+print("no1 ** no2 = ", r4)
+print("no1 * no2 = ", r5)
+print("no1 / no2 = ", r6)
+
+1
+
